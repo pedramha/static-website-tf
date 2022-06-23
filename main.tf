37d2b6aca174de425abf9375c161616ad28c0998
pedram.hamidehkhan <pedram.hamidehkhan@hashicorp.com>
2022-06-23 16:48:34 +0200

    removed local-exec provisioner as a best practice

--- main.tf
+++ main.tf
@@ -2,13 +2,16 @@ resource "aws_s3_bucket" "bucket" {
   bucket = "youruniquebucketname"
   acl    = "public-read"
 
-  provisioner "local-exec" {
-      command = "aws s3 sync static/ s3://${youruniquebucketname} --acl public-read --delete"
-
-  }
-
   website {
     index_document = "index.html"
   }
   
+}
+
+resource "aws_s3_bucket_object" "file_upload" {
+  for_each = fileset("${path.module}/static", "**")
+    bucket = aws_s3_bucket.bucket.id
+    key = each.value
+    source = "${path.module}/static/${each.value}"
+    etag = filemd5("${path.module}/static/${each.value}") 
 }
